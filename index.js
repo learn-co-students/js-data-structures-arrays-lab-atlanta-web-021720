5757779a8a1e99bf9aec3f278c78623a8d6f3a30
ChanPark <thechanmoon@gmail.com>
2020-04-01 00:16:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+// drivers.push('Ralph');
+function destructivelyAppendDriver(name)
+{
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name)
+{
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver()
+{
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver()
+{
+    drivers.shift();
+}
+
+function appendDriver(name)
+{
+    return drivers.concat(new Array(name));
+}
+
+function prependDriver(name)
+{
+    return new Array(name).concat(drivers);
+}
+
+function removeLastDriver()
+{
+    return drivers.slice(0,drivers.length -1);
+}
+
+function removeFirstDriver()
+{
+    return drivers.slice(1);
+}
